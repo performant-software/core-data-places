7b08cfe9c5d719825762cfb8ff19a9178ccff820
dleadbetter <derek@performantsoftware.com>
2024-11-04 22:13:57 -0500

    Updating build.content.mjs

--- build.content.mjs
+++ build.content.mjs
@@ -4,11 +4,13 @@ import fs from 'fs';
 
 const init = async () => {
   // Create the temporary directory
-  fs.mkdirSync('./tmp');
+  if (!fs.existsSync(process.env.TINA_LOCAL_CONTENT_PATH)) {
+    fs.mkdirSync(process.env.TINA_LOCAL_CONTENT_PATH);
+  }
 
   // Clone the content repo into the temporary directory
   const url = `https://github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}.git`;
-  child_process.execSync(`git clone ${url} ./tmp`);
+  child_process.execSync(`git clone ${url} ${process.env.TINA_LOCAL_CONTENT_PATH}`);
 };
 
 dotenv.config();
