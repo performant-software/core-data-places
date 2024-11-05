6f82b1b7f6e1f32ee51fd4463cb0a02914c69ba6
dleadbetter <derek@performantsoftware.com>
2024-11-05 06:40:46 -0500

    Updating build.content.mjs

--- build.content.mjs
+++ build.content.mjs
@@ -2,17 +2,23 @@ import child_process from 'child_process';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+const TEMP_DIR = './tmp';
+
 const init = async () => {
-  const dir = '/opt/build/repo/tina/tmp';
-
-  if (!fs.existsSync(dir)) {
-    // Create the temporary directory
-    fs.mkdirSync(dir);
-
-    // Clone the content repo into the temporary directory
-    const url = `https://github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}.git`;
-    child_process.execSync(`git clone ${url} ${dir}`);
+  // Remove the temporary directory if it exists
+  if (fs.existsSync(TEMP_DIR)) {
+    fs.rmdirSync(TEMP_DIR, { recursive: true });
   }
+
+  // Clone the content repo into the temporary directory
+  const url = `https://github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}.git`;
+  child_process.execSync(`git clone ${url} ${TEMP_DIR}`);
+
+  // Copy the "content" folder to the current directory
+  fs.cpSync(`${TEMP_DIR}/content`, './content', { recursive: true });
+
+  // Remove the temporary directory.
+  fs.rmdirSync(TEMP_DIR, { recursive: true });
 };
 
 dotenv.config();
