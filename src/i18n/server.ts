c864e4fc66b612e34440408e812c8baf73578b0d
dleadbetter <derek@performantsoftware.com>
2024-11-05 06:59:28 -0500

    Updating i18n/server to use default locale; Updating config.json with default values

--- src/i18n/server.ts
+++ src/i18n/server.ts
@@ -1,12 +1,13 @@
 import { fetchI18n } from '@backend/tina-cms';
 import { buildTranslations, getTranslation } from './utils';
+import config from '@config';
 
 /**
  * Returns the list of translations from the TinaCMS for the passed language.
  *
  * @param language
  */
-export const getTranslations = async (language: string) => {
+export const getTranslations = async (language: string = config.i18n.default_locale) => {
   const data = await fetchI18n(language) || {};
   const translations = buildTranslations(data);
 
