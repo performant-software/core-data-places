ed035b84e6d03b96ff03ae7ab474769a48523478
dleadbetter <derek@performantsoftware.com>
2024-11-04 22:21:24 -0500

    Updating build.content.mjs

--- build.content.mjs
+++ build.content.mjs
@@ -4,11 +4,13 @@ import fs from 'fs';
 
 const init = async () => {
   const dir = '/opt/build/repo/tina/tmp';
-  
+
   // Create the temporary directory
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+  if (fs.existsSync(dir)) {
+    fs.rmdirSync(dir);
   }
+
+  fs.mkdirSync(dir);
 
   // Clone the content repo into the temporary directory
   const url = `https://github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}.git`;
