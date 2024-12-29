8ba272119c4f0099f3ff1941afe6bfde87b45673
Rebecca Black <rebecca.ann.black@gmail.com>
2024-12-29 01:10:53 -0500

    initial attempts at connecting to a keycloak client

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+import NextAuth from 'next-auth';
+import { TinaAuthJSOptions } from 'tinacms-authjs';
+import { databaseClient } from '@tina/databaseClient';
+import Keycloak from 'next-auth/providers/keycloak';
+
+const NextAuthOptions = TinaAuthJSOptions({
+  databaseClient,
+  secret: import.meta.env.NEXTAUTH_SECRET!,
+  debug: true,
+  uidProp: 'username',
+  providers: [
+    Keycloak({
+      clientId: import.meta.env.AUTH_KEYCLOAK_ID,
+      clientSecret: import.meta.env.AUTH_KEYCLOAK_SECRET,
+      issuer: import.meta.env.AUTH_KEYCLOAK_ISSUER
+    })
+  ]
+});
+
+export default (req, res) => {
+  console.log(req);
+  return NextAuth(NextAuthOptions)(req, res);
+}
