129ec21126c732742974201cc248ba6fdcb6a582
Rebecca Black <rebecca.ann.black@gmail.com>
2025-01-14 16:46:25 -0500

    adding auth-astro integration

--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,4 @@
 import { Collection, defineConfig, LocalAuthProvider } from 'tinacms';
-import { TinaUserCollection, DefaultAuthJSProvider } from 'tinacms-authjs/dist/tinacms';
 import _ from 'underscore';
 import config from '../public/config.json';
 import About from './content/about';
@@ -14,7 +13,7 @@ const localContentPath = process.env.TINA_LOCAL_CONTENT_PATH;
 export default defineConfig({
   authProvider: isLocal
     ? new LocalAuthProvider()
-    : new DefaultAuthJSProvider(),
+    : new LocalAuthProvider(),
   build: {
     outputFolder: 'admin',
     publicFolder: 'public',
@@ -30,33 +29,33 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: _.compact([
-      {
-        ...TinaUserCollection,
-        fields: [
-          {
-            ...TinaUserCollection.fields[0],
-            fields: [
-              {
-                type: 'string',
-                label: 'Username',
-                name: 'username',
-                uid: true,
-                required: true,
-              },
-              {
-                type: 'string',
-                label: 'Name',
-                name: 'name',
-              },
-              {
-                type: 'string',
-                label: 'Email',
-                name: 'email',
-              },
-            ],
-          },
-        ],
-      } as Collection,
+      // {
+      //   ...TinaUserCollection,
+      //   fields: [
+      //     {
+      //       ...TinaUserCollection.fields[0],
+      //       fields: [
+      //         {
+      //           type: 'string',
+      //           label: 'Username',
+      //           name: 'username',
+      //           uid: true,
+      //           required: true,
+      //         },
+      //         {
+      //           type: 'string',
+      //           label: 'Name',
+      //           name: 'name',
+      //         },
+      //         {
+      //           type: 'string',
+      //           label: 'Email',
+      //           name: 'email',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // } as Collection,
       About,
       Settings,
       config.content?.includes('paths')
