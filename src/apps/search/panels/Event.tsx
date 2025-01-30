82a901fbf9e7354e13fc1c1cebe344f41ef097f4
dleadbetter <derek@performantsoftware.com>
2025-01-30 09:20:35 -0500

    CDP #33 - Fixing issue with event dates incorrectly displaying with empty value

--- src/apps/search/panels/Event.tsx
+++ src/apps/search/panels/Event.tsx
@@ -3,6 +3,7 @@ import { useTranslations } from '@i18n/client';
 import { useEventsService } from '@performant-software/core-data';
 import { FuzzyDate as FuzzyDateUtils } from '@performant-software/shared-components';
 import React, { useCallback } from 'react';
+import _ from 'underscore';
 
 const Event = () => {
   const { t } = useTranslations();
@@ -15,28 +16,34 @@ const Event = () => {
   /**
    * Renders the label and value for the passed date.
    */
-  const renderDate = useCallback((date, label) => (
-    <div>
-      <div
-        className='text-muted'
-      >
-        { label }
+  const renderDate = useCallback((date, label) => {
+    if (_.isEmpty(date)) {
+      return null;
+    }
+
+    return (
+      <div>
+        <div
+          className='py-1 text-muted'
+        >
+          { label }
+        </div>
+        <div
+          className='font-medium overflow-hidden text-ellipsis'
+        >
+          { FuzzyDateUtils.getDateView(date) }
+        </div>
       </div>
-      <div
-        className='font-medium overflow-hidden text-ellipsis'
-      >
-        { FuzzyDateUtils.getDateView(date) }
-      </div>
-    </div>
-  ), []);
+    );
+  }, []);
 
   return (
     <BasePanel
       name='event'
       renderItem={(event) => (
         <>
-          { event.start_date && renderDate(event.start_date, getStartDateLabel(event)) }
-          { event.end_date && renderDate(event.end_date, t('endDate')) }
+          { renderDate(event.start_date, getStartDateLabel(event)) }
+          { renderDate(event.end_date, t('endDate')) }
           { event.description && (
             <p
               className='py-2'
