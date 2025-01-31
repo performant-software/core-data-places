e070c7f4db95b53e8b9a5e26df7dae66428807a3
Rebecca Black <rebecca.ann.black@gmail.com>
2025-01-31 14:53:49 -0500

    adding console log to content collection creation

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const models = config.detail_pages;
 let cols = {};
 
 if (import.meta.env.PUBLIC_STATIC_BUILD) {
+  console.log(models)
   for (const model of models) {
     cols[model] = defineCollection({
       loader: coreDataLoader({
