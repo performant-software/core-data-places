95364e6488c47973fc9f26b9478c6e5a7fcddf1a
Camden <c@camdenmecklem.com>
2025-02-25 11:30:49 -0500

    Allow column customization via `config.json` (#80)
    * column customization
    
    * fixes and cleanup
    
    * semicolons
    
    * use tina-lock from develop

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -20,3 +20,45 @@ export const getFacetLabel = (attribute, t) => {
 
   return value;
 };
+
+/**
+ * Tests whether a string contains only integers.
+ */
+const isNumber = (str: string) => /^\d+$/.test(str);
+
+/**
+ * Gets the label for a search column as given by the result_card.attributes config array.
+ */
+export const getColumnLabel = (flattenedAtt, t) => {
+  // remove the indices from the path
+  const path = flattenedAtt
+    .split('.')
+    .filter(att => !isNumber(att))
+    .join('.');
+  
+  return getFacetLabel(path, t);
+};
+
+/**
+ * Given a flattened attribute name, e.g. myobj.hits.0.name,
+ * extracts the value from the passed hit.
+ */
+export const renderFlattenedAttribute = (hit, flattenedAtt) => {
+  const path = flattenedAtt.split('.');
+
+  let val = hit;
+
+  for (const pathItem of path) {
+    if (!val) {
+      return val;
+    }
+
+    if (isNumber(pathItem)) {
+      val = val[parseInt(pathItem)];
+    } else {
+      val = val[pathItem];
+    }
+  }
+
+  return val;
+}
