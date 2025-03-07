694be306ee85d05131458984dd903a91e7290b23
Rebecca Black <rebecca.ann.black@gmail.com>
2025-03-07 15:14:01 -0500

    added start_year and end_year labels, removed date transforms in range facet

--- src/apps/search/RangeFacet.tsx
+++ src/apps/search/RangeFacet.tsx
@@ -1,8 +1,7 @@
 import Facet from '@apps/search/Facet';
 import { Slider } from '@performant-software/core-data';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRange } from 'react-instantsearch';
-import config from '@config';
 
 interface Props {
   attribute: string,
@@ -13,28 +12,11 @@ interface Props {
 const RangeFacet = ({ attribute, className, icon }: Props) => {
   const { canRefine, start, range, refine } = useRange({ attribute });
   const { min, max } = range;
-  const isDate = useMemo(() => (config.search.date_facets?.includes(attribute)), [attribute]);
 
-  const getDisplayValue = (_value: number, _isDate: boolean) => (
-    _isDate ? (new Date(_value*1000)).getFullYear() : _value
-  );
+  const [value, setValue] = useState([min, max]);
 
-  const getValue = (_display: number, _isDate: boolean) => (
-    _isDate ? Math.round((new Date(_display, 5)).getTime()/1000) : _display
-  );
-
-  const displayMin = useMemo(() => (
-    getDisplayValue(min, isDate)
-  ), [isDate, range]);
-
-  const displayMax = useMemo(() => (
-    getDisplayValue(max, isDate)
-  ), [isDate, range])
-
-  const [value, setValue] = useState([displayMin, displayMax]);
-
-  const from = Math.max(displayMin, Number.isFinite(start[0]) ? getDisplayValue(start[0], isDate) : displayMin);
-  const to = Math.min(displayMax, Number.isFinite(start[1]) ? getDisplayValue(start[1], isDate) : displayMax);
+  const from = Math.max(min, Number.isFinite(start[0]) ? start[0] : min);
+  const to = Math.min(max, Number.isFinite(start[1]) ? start[1] : max);
 
   /**
    * Sets the value on the state when the from/to values change.
@@ -57,10 +39,10 @@ const RangeFacet = ({ attribute, className, icon }: Props) => {
       icon={icon}
     >
       <Slider
-        max={displayMax}
-        min={displayMin}
+        max={max}
+        min={min}
         onValueChange={setValue}
-        onValueCommit={(value) => refine([getValue(value[0], isDate), getValue(value[1], isDate)])}
+        onValueCommit={refine}
         value={value}
       />
     </Facet>
