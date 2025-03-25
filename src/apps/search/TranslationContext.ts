e06310c342e52d37831991444e0797dd39e18bf7
dleadbetter <derek@performantsoftware.com>
2025-03-25 11:39:19 -0400

    CDP #142 - Removing moved files (merge conflict)

--- src/apps/search/TranslationContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface TranslationContextType {
-  t(key: string, values?: { [key: string] : string | number })
-}
-
-const TranslationContext = createContext<TranslationContextType | null>(null);
-
-export default TranslationContext;
