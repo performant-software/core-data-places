8892ce1f8926e6a96aade4ed4d5a0f936eb4b278
dleadbetter <derek@performantsoftware.com>
2025-03-27 10:25:18 -0400

    CDP #142 - Updating Tina settings content collection; Updating README.md

--- tina/content/settings.ts
+++ tina/content/settings.ts
@@ -9,10 +9,85 @@ const Settings: Collection = {
   path: 'content/settings',
   format: 'json',
   fields: [{
+    name: 'branding',
+    label: 'Customization and Branding',
+    type: 'object',
+    defaultItem: () => ({
+      'primary': '#073B4C',
+      'footer_login': true
+    }),
+    fields: [{
+      type: 'string',
+      name: 'primary',
+      label: 'Primary Color',
+      ui: {
+        component: 'color',
+        //@ts-ignore
+        colorFormat: 'hex',
+        colors: ['#073B4C'],
+        widget: 'sketch',
+      }
+    }, {
+      type: 'image',
+      name: 'header_logo',
+      label: 'Header Logo'
+    }, {
+      type: 'boolean',
+      name: 'header_hide_title',
+      label: 'Hide title in header?',
+      description: 'If checked, will only display the provided logo and not the text title in the header.'
+    }, {
+      type: 'image',
+      name: 'footer_logo',
+      label: 'Footer Logo'
+    }, {
+      type: 'boolean',
+      name: 'footer_hide_title',
+      label: 'Hide title in footer?',
+      description: 'If checked, will only display the provided logo and not the text title in the footer.'
+    }, {
+      type: 'object',
+      name: 'footer_orgs',
+      label: 'Footer Organizations',
+      description: 'List of logos to display in the footer, e.g. partners or sponsors',
+      list: true,
+      fields: [{
+        type: 'image',
+        name: 'logo',
+        label: 'Logo/Image'
+      }, {
+        type: 'string',
+        name: 'url',
+        label: 'Link'
+      }, {
+        type: 'string',
+        name: 'alt',
+        label: 'Image alt text'
+      }]
+    }, {
+      type: 'boolean',
+      name: 'footer_login',
+      label: 'Display Core Data and CMS login buttons in footer?'
+    }]
+  }, {
     name: 'content',
     label: 'Content',
     type: 'string',
     list: true
+  }, {
+    name: 'core_data',
+    label: 'Core Data',
+    type: 'object',
+    fields: [{
+      name: 'url',
+      label: 'URL',
+      type: 'string'
+    }, {
+      name: 'project_ids',
+      label: 'Project IDs',
+      type: 'string',
+      list: true
+    }]
   }, {
     name: 'i18n',
     label: 'Internationalization',
@@ -79,31 +154,95 @@ const Settings: Collection = {
       },
     }]
   }, {
-    name: 'map',
-    label: 'Map',
+    name: 'result_filtering',
+    label: 'Result Filtering',
     type: 'object',
     fields: [{
-      name: 'cluster_radius',
-      label: 'Cluster Radius',
-      type: 'number'
+      name: 'events',
+      label: 'Events',
+      type: 'object',
+      fields: [{
+        name: 'exclude',
+        label: 'Exclude',
+        type: 'string',
+        list: true,
+      }]
     }, {
-      name: 'geometry',
-      label: 'Geometry',
-      type: 'string'
+      name: 'instances',
+      label: 'Instances',
+      type: 'object',
+      fields: [{
+        name: 'exclude',
+        label: 'Exclude',
+        type: 'string',
+        list: true,
+      }]
     }, {
-      name: 'max_zoom',
-      label: 'Max zoom',
-      type: 'number'
+      name: 'organizations',
+      label: 'Organizations',
+      type: 'object',
+      fields: [{
+        name: 'exclude',
+        label: 'Exclude',
+        type: 'string',
+        list: true,
+      }]
     }, {
-      name: 'zoom_to_place',
-      label: 'Zoom to place',
-      type: 'boolean'
+      name: 'people',
+      label: 'People',
+      type: 'object',
+      fields: [{
+        name: 'exclude',
+        label: 'Exclude',
+        type: 'string',
+        list: true,
+      }]
+    }, {
+      name: 'places',
+      label: 'Places',
+      type: 'object',
+      fields: [{
+        name: 'exclude',
+        label: 'Exclude',
+        type: 'string',
+        list: true,
+      }]
+    }, {
+      name: 'works',
+      label: 'Works',
+      type: 'object',
+      fields: [{
+        name: 'exclude',
+        label: 'Exclude',
+        type: 'string',
+        list: true,
+      }]
     }]
   }, {
     name: 'search',
     label: 'Search',
     type: 'object',
+    list: true,
+    ui: {
+      itemProps: (search) => {
+        return { label: search?.name }
+      }
+    },
     fields: [{
+      name: 'name',
+      label: 'Name',
+      type: 'string',
+      required: true
+    }, {
+      name: 'route',
+      label: 'Route',
+      type: 'string',
+      required: true
+    }, {
+      name: 'geosearch',
+      label: 'Geo-search',
+      type: 'boolean'
+    }, {
       name: 'facets',
       label: 'Facets',
       type: 'object',
@@ -136,248 +275,92 @@ const Settings: Collection = {
         }]
       }]
     }, {
-      name: 'geosearch',
-      label: 'Geosearch',
-      type: 'boolean'
-    }, {
       name: 'result_card',
       label: 'Result card',
       type: 'object',
       fields: [{
         name: 'title',
         label: 'Title',
-        type: 'string'
+        type: 'string',
+        required: true
       }, {
-        name: 'subtitle',
-        label: 'Subtitle',
-        type: 'string'
+        name: 'attributes',
+        label: 'Attributes',
+        type: 'object',
+        list: true,
+        fields: [{
+          name: 'name',
+          label: 'Name',
+          type: 'string',
+          required: true
+        }, {
+          name: 'icon',
+          label: 'Icon',
+          type: 'string'
+        }]
       }]
     }, {
-      name: 'route',
-      label: 'Route',
-      type: 'string'
-    },
-  {
-    name: 'result_filtering',
-    label: 'Result Filtering',
-    type: 'object',
-    fields: [
-      {
-        name: 'events',
-        label: 'Events',
-        type: 'object',
-        fields: [
-          {
-            name: 'exclude',
-            label: 'Exclude',
-            type: 'string',
-            list: true,
-          }
-        ]
-      },
-      {
-        name: 'instances',
-        label: 'Instances',
-        type: 'object',
-        fields: [
-          {
-            name: 'exclude',
-            label: 'Exclude',
-            type: 'string',
-            list: true,
-          }
-        ]
-      },
-      {
-        name: 'organizations',
-        label: 'Organizations',
-        type: 'object',
-        fields: [
-          {
-            name: 'exclude',
-            label: 'Exclude',
-            type: 'string',
-            list: true,
-          }
-        ]
-      },
-      {
-        name: 'people',
-        label: 'People',
-        type: 'object',
-        fields: [
-          {
-            name: 'exclude',
-            label: 'Exclude',
-            type: 'string',
-            list: true,
-          }
-        ]
-      },
-      {
-        name: 'places',
-        label: 'Places',
-        type: 'object',
-        fields: [
-          {
-            name: 'exclude',
-            label: 'Exclude',
-            type: 'string',
-            list: true,
-          }
-        ]
-      },
-      {
-        name: 'works',
-        label: 'Works',
-        type: 'object',
-        fields: [
-          {
-            name: 'exclude',
-            label: 'Exclude',
-            type: 'string',
-            list: true,
-          }
-        ]
-      }
-    ]
-  }]
-  }, {
-    name: 'typesense',
-    label: 'Typesense',
-    type: 'object',
-    fields: [{
-      name: 'host',
-      label: 'Host',
-      type: 'string'
-    }, {
-      name: 'port',
-      label: 'Port',
-      type: 'number'
-    }, {
-      name: 'protocol',
-      label: 'Protocol',
-      type: 'string'
-    }, {
-      name: 'api_key',
-      label: 'API key',
-      type: 'string'
-    }, {
-      name: 'index_name',
-      label: 'Index name',
-      type: 'string'
-    }, {
-      name: 'query_by',
-      label: 'Query by',
-      type: 'string'
-    }, {
-      name: 'default_sort',
-      label: 'Default sort',
-      type: 'string'
-    }, {
-      name: 'exclude_fields',
-      label: 'Exclude fields',
-      type: 'string',
-      list: true
-    }, {
-      name: 'facets',
-      label: 'Facets',
+      name: 'typesense',
+      label: 'Typesense',
       type: 'object',
       fields: [{
-        name: 'exclude',
-        label: 'Exclude',
+        name: 'host',
+        label: 'Host',
+        type: 'string'
+      }, {
+        name: 'port',
+        label: 'Port',
+        type: 'number'
+      }, {
+        name: 'protocol',
+        label: 'Protocol',
+        type: 'string'
+      }, {
+        name: 'api_key',
+        label: 'API key',
+        type: 'string'
+      }, {
+        name: 'index_name',
+        label: 'Index name',
+        type: 'string'
+      }, {
+        name: 'query_by',
+        label: 'Query by',
+        type: 'string'
+      }, {
+        name: 'default_sort',
+        label: 'Default sort',
+        type: 'string'
+      }, {
+        name: 'exclude_fields',
+        label: 'Exclude fields',
         type: 'string',
         list: true
       }, {
-        name: 'include',
-        label: 'Include',
-        type: 'string',
-        list: true
-      }]
-    }, {
-      name: 'overrides',
-      label: 'Overrides',
-      type: 'object',
-      fields: [{
-        name: 'geoLocationField',
-        label: 'Geo-location field',
-        type: 'string'
-      }]
-    }]
-  }, {
-    name: 'core_data',
-    label: 'Core Data',
-    type: 'object',
-    fields: [{
-      name: 'url',
-      label: 'URL',
-      type: 'string'
-    }, {
-      name: 'project_ids',
-      label: 'Project IDs',
-      type: 'string',
-      list: true
-    }]
-  }, {
-    name: 'branding',
-    label: 'Customization and Branding',
-    type: 'object',
-    defaultItem: () => ({
-      'primary': '#073B4C',
-      'footer_login': true
-    }),
-    fields: [{     
-      type: 'string',
-      name: 'primary',
-      label: 'Primary Color',
-      ui: {
-        component: 'color',
-        //@ts-ignore
-        colorFormat: 'hex',
-        colors: ['#073B4C'],
-        widget: 'sketch',
-      }
-    }, {
-      type: 'image',
-      name: 'header_logo',
-      label: 'Header Logo'
-    }, {
-      type: 'boolean',
-      name: 'header_hide_title',
-      label: 'Hide title in header?',
-      description: 'If checked, will only display the provided logo and not the text title in the header.'
-    }, {
-      type: 'image',
-      name: 'footer_logo',
-      label: 'Footer Logo'
-    }, {
-      type: 'boolean',
-      name: 'footer_hide_title',
-      label: 'Hide title in footer?',
-      description: 'If checked, will only display the provided logo and not the text title in the footer.'
-    }, {
-      type: 'object',
-      name: 'footer_orgs',
-      label: 'Footer Organizations',
-      description: 'List of logos to display in the footer, e.g. partners or sponsors',
-      list: true,
-      fields: [{
-        type: 'image',
-        name: 'logo',
-        label: 'Logo/Image'
+        name: 'facets',
+        label: 'Facets',
+        type: 'object',
+        fields: [{
+          name: 'exclude',
+          label: 'Exclude',
+          type: 'string',
+          list: true
+        }, {
+          name: 'include',
+          label: 'Include',
+          type: 'string',
+          list: true
+        }]
       }, {
-        type: 'string',
-        name: 'url',
-        label: 'Link'
-      }, {
-        type: 'string',
-        name: 'alt',
-        label: 'Image alt text'
+        name: 'overrides',
+        label: 'Overrides',
+        type: 'object',
+        fields: [{
+          name: 'geoLocationField',
+          label: 'Geo-location field',
+          type: 'string'
+        }]
       }]
-    }, {
-      type: 'boolean',
-      name: 'footer_login',
-      label: 'Display Core Data and CMS login buttons in footer?'
     }]
   }],
   ui: {
