f581d16e132e4eeaf038dcfe291ca310c9b1ffb9
dleadbetter <derek@performantsoftware.com>
2025-04-07 20:31:41 -0400

    CDP #163 - Adding error handling to TimelineInput

--- tina/components/TimelineInput.tsx
+++ tina/components/TimelineInput.tsx
@@ -1,6 +1,6 @@
 import config from '@config';
 import { FuzzyDate as FuzzyDateUtils, ObjectJs as ObjectUtils } from '@performant-software/shared-components';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { wrapFieldsWithMeta } from 'tinacms';
 import _ from 'underscore';
 import JsonUpload from './JsonUpload';
@@ -11,33 +11,40 @@ const INDEX_END_DATE = 1;
 const SECOND_TO_MILLISECONDS = 1000;
 
 const TimelineInput = wrapFieldsWithMeta((props) => {
+  const [error, setError] = useState<string | undefined>();
+
   /**
    * Returns the year range for the passed date array.
    */
-  const getYearRange = useCallback((dateArray) => (
-    FuzzyDateUtils.getYearRange({
+  const getYearRange = useCallback((dateArray) => {
+    if (!dateArray) {
+      return [];
+    }
+
+    return FuzzyDateUtils.getYearRange({
       start_date: dateArray[INDEX_START_DATE] * SECOND_TO_MILLISECONDS,
       end_date: dateArray[INDEX_END_DATE] * SECOND_TO_MILLISECONDS
-    })
-  ), []);
+    });
+}, []);
 
   /**
-   * Removes all of the extraneous data from the uploaded JSON.
+   * Sets the passed records as the input value.
    */
-  const onChange = useCallback((data) => {
-    const { name, data: records } = JSON.parse(data);
-    const searchConfig = _.findWhere(config.search, { name });
-
+  const setInputValue = useCallback((records, searchConfig) => {
     const events = [];
 
     let min;
     let max;
 
     _.each(records, (record) => {
-      const eventData = ObjectUtils.getNestedValue(record, searchConfig.timeline.event_path || '');
+      let eventData = ObjectUtils.getNestedValue(record, searchConfig.timeline?.event_path);
+
+      if (eventData && !_.isArray(eventData)) {
+        eventData = [eventData];
+      }
 
       _.each(eventData, (event) => {
-        events.push(event);
+        events.push(_.pick(event, 'name', 'start_date', 'end_date'));
 
         const startRange = getYearRange(event.start_date);
         const endRange = getYearRange(event.end_date);
@@ -58,11 +65,34 @@ const TimelineInput = wrapFieldsWithMeta((props) => {
     props.input.onChange(JSON.stringify({ name, events, min, max }));
   }, [getYearRange]);
 
+  /**
+   * Removes all of the extraneous data from the uploaded JSON.
+   */
+  const onChange = useCallback((data) => {
+    const { name, data: records } = JSON.parse(data);
+    const searchConfig = _.findWhere(config.search, { name });
+
+    if (_.has(searchConfig.timeline, 'event_path')) {
+      setInputValue(records, searchConfig);
+    } else {
+      setError('The uploaded dataset does not support the "Timeline" visualization.');
+    }
+  }, [setInputValue]);
+
   return (
-    <JsonUpload
-      onChange={onChange}
-      value={props.input.value}
-    />
+    <>
+      { error && (
+        <div
+          className='text-lg'
+        >
+          { error }
+        </div>
+      )}
+      <JsonUpload
+        onChange={onChange}
+        value={props.input.value}
+      />
+    </>
   );
 });
 
