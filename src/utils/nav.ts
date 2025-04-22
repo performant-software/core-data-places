84749c914812b4abc1df98920dd2091fbc686742
dleadbetter <derek@performantsoftware.com>
2025-04-22 10:55:21 -0400

    CDP #179 - Adding getHomepage function to utils/nav

--- src/utils/nav.ts
+++ src/utils/nav.ts
@@ -2,6 +2,18 @@ import { fetchPages } from '@backend/tina';
 import _ from 'underscore';
 
 /**
+ * Returns the first page flagged as "home_page".
+ *
+ * @param locale
+ */
+export const getHomepage = async (locale: string) => {
+  const pages = await fetchPages(locale, { filter: { home_page: { eq: true } } });
+  const [page, ] = pages;
+
+  return page;
+};
+
+/**
  * Returns the set of all non-homepage pages.
  */
 export const getPages = async (locale: string) => {
