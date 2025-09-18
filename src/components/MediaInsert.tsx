3dd899fe5577da952c456e8f432adbc509515278
Rebecca Black <rebecca.ann.black@gmail.com>
2025-09-18 14:22:12 -0400

    WIP

--- src/components/MediaInsert.tsx
+++ src/components/MediaInsert.tsx
@@ -1,7 +1,7 @@
 import Viewer from '@samvera/clover-iiif/viewer'
 
 const MediaInsert = (props: any) => {
-  return (
+  return props?.media?.manifest_url && (
     <div className='mx-auto w-full'>
       <Viewer
         iiifContent={props?.media?.manifest_url}
