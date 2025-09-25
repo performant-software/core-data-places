914d695da9b104a60225157da1ae62b8dde7cd29
Rebecca Black <rebecca.ann.black@gmail.com>
2025-09-25 00:56:26 -0400

    add more flexibility and error handling to data loaders

--- src/loaders/coreData/helpers.ts
+++ src/loaders/coreData/helpers.ts
@@ -1,12 +1,23 @@
 import config from '@config';
 import type { LoaderContext } from 'astro/loaders';
 import _ from 'underscore';
+import { CONTENT_MODE } from 'astro:env/client';
 
 const RELATIONSHIP_ATTRIBUTES = [
   'uuid',
   'project_model_relationship_uuid',
-  'project_model_relationship_inverse'
+  'project_model_relationship_inverse',
 ];
+
+//amount to pause between loop iterations, in ms
+const DELAY = 10;
+
+/**
+ * Helper function to add delay between loop iterations
+ *
+ * @param ms
+ */
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * To avoid duplicating data, only store the necessary attributes for related records. The full record will be fetched
@@ -21,9 +32,18 @@ const filterRelated = (records) => {
 export const createLoader = (name, Service, [indexName, showName]) => ({
   name,
   load: async (context: LoaderContext): Promise<void> => {
-    const { generateDigest, logger, parseData, store } = context;
+    const { generateDigest, logger, meta, parseData, store } = context;
 
-    const service = new Service(config.core_data.url, config.core_data.project_ids);
+    if (CONTENT_MODE === 'refresh') {
+      logger.info('Clearing the data store...');
+      store.clear();
+      logger.info(`Data store ${name} cleared.`);
+    }
+
+    const service = new Service(
+      config.core_data.url,
+      config.core_data.project_ids
+    );
     const params = { per_page: 100 };
 
     const startTime = Date.now();
@@ -50,57 +70,66 @@ export const createLoader = (name, Service, [indexName, showName]) => ({
       }
 
       for (const { uuid } of listRecords) {
-        const recordStart = Date.now();
-        logger.info(`Loading ${uuid} ${count} of ${total}`);
+        if (CONTENT_MODE === 'add' && store.get(uuid)) {
+          logger.info(`Record ${uuid} already exists. Skipping.`);
+          continue;
+        }
+        try {
+          const recordStart = Date.now();
+          logger.info(`Loading ${uuid} ${count} of ${total}`);
 
-        const [
-          response,
-          { events },
-          { instances },
-          { items },
-          { media_contents: mediaContents },
-          { organizations },
-          { people },
-          { places },
-          { taxonomies },
-          { works },
-          manifests
-        ] = await Promise.all([
-          service.fetchOne(uuid),
-          service.fetchRelatedEvents(uuid, params),
-          service.fetchRelatedInstances(uuid, params),
-          service.fetchRelatedItems(uuid, params),
-          service.fetchRelatedMedia(uuid, params),
-          service.fetchRelatedOrganizations(uuid, params),
-          service.fetchRelatedPeople(uuid, params),
-          service.fetchRelatedPlaces(uuid, params),
-          service.fetchRelatedTaxonomies(uuid, params),
-          service.fetchRelatedWorks(uuid, params),
-          service.fetchRelatedManifests(uuid, params)
-        ]);
-
-        const record = response[showName];
-
-        records.push({
-          ...record,
-          relatedRecords: {
-            events: filterRelated(events),
-            instances: filterRelated(instances),
-            items: filterRelated(items),
+          const [
+            response,
+            { events },
+            { instances },
+            { items },
+            { media_contents: mediaContents },
+            { organizations },
+            { people },
+            { places },
+            { taxonomies },
+            { works },
             manifests,
-            mediaContents,
-            organizations: filterRelated(organizations),
-            people: filterRelated(people),
-            places: filterRelated(places),
-            taxonomies,
-            works: filterRelated(works)
-          }
-        });
+          ] = await Promise.all([
+            service.fetchOne(uuid),
+            service.fetchRelatedEvents(uuid, params),
+            service.fetchRelatedInstances(uuid, params),
+            service.fetchRelatedItems(uuid, params),
+            service.fetchRelatedMedia(uuid, params),
+            service.fetchRelatedOrganizations(uuid, params),
+            service.fetchRelatedPeople(uuid, params),
+            service.fetchRelatedPlaces(uuid, params),
+            service.fetchRelatedTaxonomies(uuid, params),
+            service.fetchRelatedWorks(uuid, params),
+            service.fetchRelatedManifests(uuid, params),
+          ]);
 
-        const recordEnd = Date.now();
-        logger.info(`${uuid} complete in ${recordEnd - recordStart}ms`);
+          const record = response[showName];
 
-        count += 1;
+          records.push({
+            ...record,
+            relatedRecords: {
+              events: filterRelated(events),
+              instances: filterRelated(instances),
+              items: filterRelated(items),
+              manifests,
+              mediaContents,
+              organizations: filterRelated(organizations),
+              people: filterRelated(people),
+              places: filterRelated(places),
+              taxonomies,
+              works: filterRelated(works),
+            },
+          });
+
+          const recordEnd = Date.now();
+          logger.info(`${uuid} complete in ${recordEnd - recordStart}ms`);
+        } catch (error) {
+          logger.info(`Error loading record ${uuid}: ${error}`);
+        } finally {
+          await delay(DELAY);
+          count += 1;
+        }
       }
     }
 
@@ -110,10 +139,15 @@ export const createLoader = (name, Service, [indexName, showName]) => ({
     for (const record of records) {
       const data = await parseData({ id: record.uuid, data: record });
       const digest = generateDigest(data);
-      store.set({ id: record.uuid, data, digest });
+      const updated = store.set({ id: record.uuid, data, digest }); //this will return false if the digests match, and the record will not be set
+      if (updated) {
+        meta.set('lastModified', new Date().toISOString());
+      }
     }
 
     const endTime = Date.now();
-    logger.info(`Datastore updated. ${count} record(s) in ${endTime - startTime}ms`);
-  }
+    logger.info(
+      `Datastore updated. ${count - 1} record(s) in ${endTime - startTime}ms`
+    );
+  },
 });
