e85b5f3bc48be5fde7b6c8998f729947e47d3654
Camden Mecklem <c@camdenmecklem.com>
2025-12-05 15:03:57 -0500

    config change and new relateditems

--- src/utils/detailPages.ts
+++ src/utils/detailPages.ts
@@ -6,7 +6,7 @@ type Models = 'events' | 'instances' | 'items' | 'mediaContents' | 'organization
 export const getDetailPagePaths = (async (config: any, model: Models) => {
   let routes = [];
 
-  if (!config.detail_pages || !config.detail_pages.includes(model)) {
+  if (!config.detail_pages?.models || !config.detail_pages.models.includes(model)) {
     return routes;
   }
 
