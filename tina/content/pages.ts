2e72afae500643638d719b703b140e9810be6380
Rebecca Black <rebecca.ann.black@gmail.com>
2025-12-14 15:34:11 -0500

    transparent header options; other small tweaks

--- tina/content/pages.ts
+++ tina/content/pages.ts
@@ -112,6 +112,10 @@ const Pages: Collection = {
     label: 'Navigation Menu',
     type: 'boolean'
   }, {
+    name: 'transparent',
+    label: 'Make top navbar transparent?',
+    type: 'boolean'
+  }, {
     name: 'sections',
     label: 'Sections',
     type: 'object',
