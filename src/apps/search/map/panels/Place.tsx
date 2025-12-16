a4bdfe01ba7dc4d6dd192082085f85de19525d89
Camden Mecklem <c@camdenmecklem.com>
2025-12-16 15:12:54 -0500

    cleanup

--- src/apps/search/map/panels/Place.tsx
+++ src/apps/search/map/panels/Place.tsx
@@ -25,7 +25,7 @@ const Place = (props: Props) => {
    * Resolves the URL for the detail page.
    */
   const resolveDetailPageUrl = useCallback((place) => {
-    if (place && config.detail_pages?.models && config.detail_pages?.models?.includes('places')) {
+    if (place && config.detail_pages?.models?.includes('places')) {
       return `/${lang}/places/${place.uuid}`;
     }
   }, [config, lang]);
