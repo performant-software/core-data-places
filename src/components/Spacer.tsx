b1f5faa18850dc8eccd0480876748610a5dd595e
Rebecca Black <rebecca.ann.black@gmail.com>
2026-01-05 10:56:10 -0500

    QA styling tweaks

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -4,13 +4,15 @@ import { toBackgroundClass } from '../utils/pageBuilder';
 interface SpacerProps {
   size: string;
   color?: string;
+  thick?: boolean;
 }
 
 const Spacer = (props: SpacerProps) => (
   <div
     className={clsx(
       'w-full',
-      { 'h-[2px]': !!props.color },
+      { 'h-[1px]': !!props.color && !props.thick },
+      { 'h-[2px]': !!props.color && props.thick },
       { 'my-4 lg:my-8': props.size === 'small' },
       { 'my-8 lg:my-16': props.size === 'medium' },
       { 'my-16 lg:my-32': props.size === 'large' },
