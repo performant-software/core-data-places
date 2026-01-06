eb2a9f66222bf998cd51f0f730890d61bce68faa
Rebecca Black <rebecca.ann.black@gmail.com>
2026-01-06 11:13:03 -0500

    button styling fix

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -15,8 +15,8 @@ interface Props {
 const LinkButton = ({ className, content, href, ...props }: Props) => (
   <a
     className={clsx(
-      'link-button inline-flex mt-4 rounded-md px-6 py-3 hover:bg-opacity-95 cursor-pointer flex-row gap-4 items-center no-underline',
-      'hover:bg-[linear-gradient(rgba(0,0,0,0.15),rgba(0,0,0,0.15))] transition duration-300',
+      'link-button inline-flex mt-4 rounded-md hover:bg-opacity-95 cursor-pointer flex-row gap-4 items-center no-underline',
+      { 'px-6 py-3 hover:bg-[linear-gradient(rgba(0,0,0,0.15),rgba(0,0,0,0.15))] transition duration-300': props.color || props.border },
       'focus:outline-none',
       props.color && 'focus:shadow-[0_1px_2px_0_rgba(0,0,0,0.05),0_0_0_2px_var(--color-layout),0_0_0_4px_var(--color-secondary)]',
       { 'hover:underline hover:underline-offset-[6px] hover:decoration-2': !props.color && !props.border },
