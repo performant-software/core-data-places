80f5b69331a113e9a6c3a1fe03c831dfbf899bda
Rebecca Black <rebecca.ann.black@gmail.com>
2026-01-06 11:12:42 -0500

    adding xs option to spacers

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -13,6 +13,7 @@ const Spacer = (props: SpacerProps) => (
       'w-full',
       { 'h-[1px]': !!props.color && !props.thick },
       { 'h-[2px]': !!props.color && props.thick },
+      { 'my-3 lg:my-5': props.size === 'xs'},
       { 'my-4 lg:my-8': props.size === 'small' },
       { 'my-8 lg:my-16': props.size === 'medium' },
       { 'my-16 lg:my-32': props.size === 'large' },
