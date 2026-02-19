f0edf226f8e557f2485870127afd17a45c6c93a8
dleadbetter <derek@performantsoftware.com>
2026-02-19 09:28:52 -0500

    CDP #542 - Adding HTML output to Playwright tests; Updating README.md

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', { outputFile: 'test-results/test-results.json' }]
+    ['json', { outputFile: 'test-results/test-results.json' }],
+    ['html', { open: 'never' }]
   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
