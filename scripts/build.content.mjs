2bbc29ab7391e3b51e757efedebfee67122ee537
Camden Mecklem <c@camdenmecklem.com>
2026-03-24 17:49:13 -0400

    admin redirect

--- scripts/build.content.mjs
+++ scripts/build.content.mjs
@@ -20,6 +20,15 @@ export const fetchContent = async () => {
   // Copy the "content" folder to the current directory
   fs.cpSync(`${TEMP_DIR}/content`, './content', { recursive: true });
 
+  // Append any custom Netlify config to the main one
+  // (mainly used for redirecting the admin site to a Performant Studio subdomain)
+  if (fs.existsSync('./content/netlify.toml')) {
+    const customConfig = fs.readFileSync('./content/netlify.toml', 'utf8');
+    const existingConfig = fs.readFileSync('./netlify.toml', 'utf8');
+    const newConfig = `${existingConfig}\n${customConfig}`;
+    fs.writeFileSync('./netlify.toml', newConfig);
+  }
+
   // Remove the temporary directory.
   fs.rmSync(TEMP_DIR, { recursive: true });
 };
