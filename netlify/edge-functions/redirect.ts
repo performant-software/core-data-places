2fc0ff6a221f3993bc9c5e072784e39a253df6ba
Camden Mecklem <c@camdenmecklem.com>
2026-03-27 08:57:58 -0400

    admin redirect

---
+++ netlify/edge-functions/redirect.ts
@@ -0,0 +1,31 @@
+import type { Config, Context } from '@netlify/edge-functions';
+
+const publicDomain = Netlify.env.get('PUBLIC_DOMAIN');
+const adminDomain = Netlify.env.get('ADMIN_DOMAIN');
+
+export default async (request: Request, context: Context) => {
+  if (request.headers.get('sec-fetch-dest') === 'iframe') {
+    return context.next();
+  }
+
+  const url = new URL(request.url);
+
+  console.log(url.hostname, url.pathname);
+
+  if (url.hostname === publicDomain && url.pathname.startsWith('/admin')) {
+    url.hostname = adminDomain;
+    url.port = '';
+    return Response.redirect(url.toString(), 301);
+  }
+
+  if (url.hostname === adminDomain && !url.pathname.startsWith('/admin')) {
+    url.hostname = publicDomain;
+    return Response.redirect(url.toString(), 301);
+  }
+
+  return context.next();
+};
+
+export const config: Config = {
+  path: '/*',
+};
