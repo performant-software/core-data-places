9b545da913084e9387f6e3b560928111893f847b
Camden Mecklem <c@camdenmecklem.com>
2026-03-27 12:44:14 -0400

    edge function fixes

--- netlify/edge-functions/redirect.ts
+++ netlify/edge-functions/redirect.ts
@@ -3,24 +3,34 @@ import type { Config, Context } from '@netlify/edge-functions';
 const publicDomain = Netlify.env.get('PUBLIC_DOMAIN');
 const adminDomain = Netlify.env.get('ADMIN_DOMAIN');
 
+const secFetchHeaders = [
+  'iframe',
+  'empty'
+]
+
+const adminPaths = [
+  '/admin',
+  '/api/tina'
+]
+
 export default async (request: Request, context: Context) => {
   if (!publicDomain || !adminDomain) {
     return context.next();
   }
 
-  if (request.headers.get('sec-fetch-dest') === 'iframe') {
+  if (secFetchHeaders.includes(request.headers.get('sec-fetch-dest'))) {
     return context.next();
   }
 
   const url = new URL(request.url);
 
-  if (url.hostname === publicDomain && url.pathname.startsWith('/admin')) {
+  if (url.hostname === publicDomain && adminPaths.some((path) => url.pathname.startsWith(path))) {
     url.hostname = adminDomain;
     url.port = '';
     return Response.redirect(url.toString(), 301);
   }
 
-  if (url.hostname === adminDomain && !url.pathname.startsWith('/admin')) {
+  if (url.hostname === adminDomain && !adminPaths.some(path => url.pathname.startsWith(path))) {
     url.hostname = publicDomain;
     return Response.redirect(url.toString(), 301);
   }
