16811fb15c53296e8e93a02c7515fbdd2c17cb24
Rebecca Black <rebecca.ann.black@gmail.com>
2026-07-14 17:20:34 -0400

    more cleanup

--- src/lib/islands.ts
+++ src/lib/islands.ts
@@ -4,6 +4,10 @@ import type { QueryResult } from '@tinacms/astro/data';
 import { fetchPageResponse } from '@backend/tina';
 import { PagesQuery } from '@root/tina/__generated__/types';
 import Page from '@apps/pages/Page.astro';
+import { getTranslations } from '@backend/i18n';
+import config from '@config';
+
+const { t } = await getTranslations(config.i18n.default_locale);
 
 export const islands: IslandRegistry = {
   page: {
@@ -12,7 +16,8 @@ export const islands: IslandRegistry = {
     wrapper: { tag: 'main' },
     propsFromData: (data) => ({
       page: (data as QueryResult<PagesQuery>).data?.pages,
-      isTinaPreview: true
+      isTinaPreview: true,
+      t
     }),
   }
 };
