415d52de828dfb31fcc8deac30813e78aca0a52d
Rebecca Black <rebecca.ann.black@gmail.com>
2026-07-26 15:54:21 -0400

    update edithistory to only display for admins

--- tina/components/EditHistory.tsx
+++ tina/components/EditHistory.tsx
@@ -1,21 +1,50 @@
-import { wrapFieldsWithMeta } from 'tinacms';
+import { useCMS, wrapFieldsWithMeta } from 'tinacms';
 import _ from 'underscore';
+import { getUserRole } from '../utils/getUserRole';
+import { useState } from 'react';
 
 const EditHistory = wrapFieldsWithMeta((props: any) => {
+  const [showMore, setShowMore] = useState<boolean>(false);
+  const cms = useCMS();
+  const { isAdmin } = getUserRole(cms);
+
   return (
     <div>
       {
-        !props.input.value?.length && (
+        !isAdmin && (
+          <p className='italic'>Unavailable</p>
+        )
+      }
+      {
+        isAdmin && !props.input.value?.length && (
           <p className='italic'>None yet.</p>
         )
       }
       {
-        props.input.value && props.input.value.length > 0 && _.map(props.input.value, (entry: any) => (
-          <div className='w-full flex flex-row justify-between'>
-            <p>{entry.user_email}</p>
-            <p>{new Date(entry.timestamp).toLocaleString()}</p>
-          </div>
-        ))
+        isAdmin && props.input.value && props.input.value.length > 0 && (
+          <>
+           { _.map(props.input.value.slice(0,5), (entry: any) => (
+            <div className='w-full flex flex-row justify-between'>
+              <p>{entry.user_email}</p>
+              <p>{new Date(entry.timestamp).toLocaleString()}</p>
+            </div>
+            )) }
+            { showMore && _.map(props.input.value.slice(5,), (entry: any) => (
+              <div className='w-full flex flex-row justify-between'>
+                <p>{entry.user_email}</p>
+                <p>{new Date(entry.timestamp).toLocaleString()}</p>
+              </div>
+            ))}
+            { props.input.value.length > 5 && (
+              <button 
+                className='px-3 py-2 rounded-lg bg-white hover:bg-gray-200 border border-gray-600 my-2 flex items-center justify-center cursor-pointer'
+                onClick={() => setShowMore(current => !current)}
+              >
+                <p>{ showMore ? 'Show Less' : 'Show More' }</p>
+              </button>
+            )}
+          </>
+        )
       }
     </div>
   )
